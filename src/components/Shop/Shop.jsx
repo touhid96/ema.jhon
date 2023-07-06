221d1cdfa75d32ec9761ca848b38217f08578e6c
touhid <touhid.islam96@gmail.com>
2023-07-06 22:52:29 +0200

    added some new

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -12,10 +13,14 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
-
+  useEffect(() => {
+    const getLocalData = getShoppingCart();
+    console.log(getLocalData);
+  }, []);
   const handleAddtoCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
   return (
     <div className="shop-container">
